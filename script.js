e506b600f7a157aa1e9b48547a931d13bf67a1b2
Jari Peltola <jena9988@gmail.com>
2024-02-24 14:41:48 +0200

    Add Script

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleFullScreen() {
+    var img = document.querySelector('.thumbnail');
+    img.classList.toggle('fullscreen-image');
+}
+
+function toggleFullScreen(img) {
+    img.classList.toggle('fullscreen-image');
+}
